1df4adf1294710003c0e05db27294c3248f2c6f6
Raphael von der Grün <raphinesse@gmail.com>
2021-10-15 18:40:55 +0200

    fix(main.js): color of 404 status in console logs

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ function CordovaServe () {
     // Attach this before anything else to provide status output
     this.app.use((req, res, next) => {
         res.on('finish', function () {
-            const color = this.statusCode === '404' ? chalk.red : chalk.green;
+            const color = this.statusCode === 404 ? chalk.red : chalk.green;
             let msg = `${color(this.statusCode)} ${this.req.originalUrl}`;
             const encoding = this.getHeader('content-encoding');
             if (encoding) {
